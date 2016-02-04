bc29d0dd84b2c2bd4753a1c43a4f982927e87d2e
irwinj <jirwin2@gmail.com>
2016-02-04 11:07:16 -0800

    adding rails models

---
+++ rod_server/app/models/pole.rb
@@ -0,0 +1,2 @@
+class Pole < ActiveRecord::Base
+end
---
+++ rod_server/spec/factories/poles.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :pole do
+    name "MyString"
+    rodmodel "MyString"
+    picture "MyString"
+    type ""
+    length ""
+    pieces ""
+    lureweight ""
+    lineweight ""
+    pricerange ""
+    power "MyString"
+  end
+end

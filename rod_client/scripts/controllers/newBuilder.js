25b5e7b98b21276ff35cddc35e939a1a1360cd3e
erikapallasmoore <erikapallasmoore@gmail.com>
2016-02-04 23:44:32 -0800

    updated controller for newBuilder - need to test

--- rod_client/scripts/controllers/newBuilder.js
+++ rod_client/scripts/controllers/newBuilder.js
@@ -1,6 +1,53 @@
 'use strict';
  
 angular.module('rodBrokerApp')
-  .controller('NewBuilderCtrl', ['$scope', 'NewBuilder', function ($scope, NewBuilder) {
+  .controller('NewBuilderCtrl', ['$scope', 'NewBuilder', '$location', 'Post', 'Auth', function ($scope, NewBuilder, $location, Post, Auth) {
     $scope.newBuilder = NewBuilder.query();
+      if (!Auth.isLoggedIn()) {
+    console.log('DENY');
+    $location.path('/login');
+    return;
+  }
+
+  $scope.post = {
+    name: '',
+    address: '',
+    email: '',
+    phone: '',
+    species: '',
+    rod: '',
+    blank: '',
+    description: '',
+    username: Auth.currentUser().username
+  };
+
+  $scope.createPost = function() {
+    Post.save($scope.post, function success(data) {
+      $location.path('/');
+    }, function error(data) {
+      console.log(data);
+    });
+  }
   }]);
+
+//   .controller('NewCtrl', ['$scope', '$location', 'Post', 'Auth', function($scope, $location, Post, Auth) {
+//   if (!Auth.isLoggedIn()) {
+//     console.log('DENY');
+//     $location.path('/login');
+//     return;
+//   }
+
+//   $scope.post = {
+//     title: '',
+//     body: '',
+//     username: Auth.currentUser().username
+//   };
+
+//   $scope.createPost = function() {
+//     Post.save($scope.post, function success(data) {
+//       $location.path('/');
+//     }, function error(data) {
+//       console.log(data);
+//     });
+//   }
+// }])

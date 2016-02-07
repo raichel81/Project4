15930d8f1adab33a0a13ec1ae5774070890e04b7
irwinj <jirwin2@gmail.com>
2016-02-07 13:55:38 -0800

    new builder page is creating builders

--- rod_client/scripts/controllers/new.js
+++ rod_client/scripts/controllers/new.js
@@ -2,27 +2,17 @@
  
 angular.module('rodBrokerApp')
 .controller('NewCtrl', ['$scope', '$location', 'Builder', 'Auth', function($scope, $location, Builder, Auth) {
-  if (!Auth.isLoggedIn()) {
-    console.log('DENY');
-    $location.path('/login');
-    return;
-  }
+  // if (!Auth.isLoggedIn()) {
+  //   console.log('DENY');
+  //   $location.path('/login');
+  //   return;
+  // }
 
-  $scope.builder = {
-    name: '',
-    email: '',
-    phone: '',
-    bio: '',    
-    location: '',
-    minprice: '',    
-    maxprice: '',
-    ship: '',    
-    rodtype: '',   
-    species: '',
-    poleblank: '',
-    username: Auth.currentUser().username
-  };
-
+  // $scope.builder = {
+  //   title: '',
+  //   body: '',
+  //   username: Auth.currentUser().username
+  // };
 
   $scope.createBuilder = function() {
     Builder.save($scope.builder, function success(data) {
@@ -31,4 +21,37 @@ angular.module('rodBrokerApp')
       console.log(data);
     });
   }
-}]);
+}])
+
+
+// 'use strict';
+ 
+// angular.module('rodBrokerApp')
+//   .controller('NewBuilderCtrl', ['$scope', 'NewBuilder', '$location', 'Post', 'Auth', function ($scope, NewBuilder, $location, Post, Auth) {
+//     $scope.newBuilder = NewBuilder.query();
+//       if (!Auth.isLoggedIn()) {
+//     console.log('DENY');
+//     $location.path('/login');
+//     return;
+//   }
+
+//   $scope.post = {
+//     name: '',
+//     address: '',
+//     email: '',
+//     phone: '',
+//     species: '',
+//     rod: '',
+//     blank: '',
+//     description: '',
+//     username: Auth.currentUser().username
+//   };
+
+//   $scope.createPost = function() {
+//     Post.save($scope.post, function success(data) {
+//       $location.path('/');
+//     }, function error(data) {
+//       console.log(data);
+//     });
+//   }
+//   }]);

cdcd5b5cf9d05a654b47922bb8dd22de10ed3977
raichel81 <rai.chel.ann@gmail.com>
2016-02-07 18:27:20 -0800

    change to postgres from sqlite

--- rod_server/db/schema.rb
+++ rod_server/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160208012055) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "builders", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
@@ -35,7 +38,7 @@ ActiveRecord::Schema.define(version: 20160208012055) do
     t.datetime "updated_at"
   end
 
-  add_index "groups", ["name"], name: "index_groups_on_name", unique: true
+  add_index "groups", ["name"], name: "index_groups_on_name", unique: true, using: :btree
 
   create_table "poles", force: :cascade do |t|
     t.string   "name"
@@ -60,7 +63,7 @@ ActiveRecord::Schema.define(version: 20160208012055) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
